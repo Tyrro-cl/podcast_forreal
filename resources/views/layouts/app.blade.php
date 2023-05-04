d26a6d5cd8da92c48cce5d9499328a4ac8350068
Tamir Rojas <tyr.rojas@gmail.com>
2023-05-04 23:44:00 +0200

    reviewing how components, layouts and slots works in order to start creating the admin layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900"> <!-- inside this div, the include method invokes the dashboard component  -->
             @include('layouts.navigation')
 
             <!-- Page Heading -->
